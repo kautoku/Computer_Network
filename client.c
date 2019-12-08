608e20cec56da7a11e159aadd4576c75c2a9558d
kautoku <55758600+kautoku@users.noreply.github.com>
2019-12-08 22:31:48 +0800

    Update client.c

--- client.c
+++ client.c
@@ -4,7 +4,7 @@
 #include<arpa/inet.h>	//function inet_addr : convert IP address
 #include <unistd.h> //read
 
-int main()//int argc , char *argv[])
+int main(int argc , char *argv[])
 {
 	int socket_desc;
 	struct sockaddr_in server;
